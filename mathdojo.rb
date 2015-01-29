4a09c5e0f42481e049cb4be7c9d6810f859a4482
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-28 16:15:21 -0800

    Phase one complete!

--- mathdojo.rb
+++ mathdojo.rb
@@ -1,16 +1,22 @@
 class MathDojo
+	def initialize
+		@total = 0
+	end
  	def add *numbers
-		@sum = *numbers.inject(:+)
-		puts @sum
+		@sum = numbers.inject(:+)
+		@total += @sum
 		self	
 	end
 	def subtract *numbers
-		@difference = *numbers.inject(:-)
-		puts @difference
+		@difference = numbers.inject(:+)
+		@total -= @difference
 		self
+	end
+	def result
+		return @total
 	end
 end
 
-# puts MathDojo.new.add(2,6)
-# puts MathDojo.new.subtract(2,3)
-puts MathDojo.new.add(2).add(2,5).subtract(3,2)
+puts MathDojo.new.add(2,6)
+puts MathDojo.new.subtract(2,3)
+puts MathDojo.new.add(2).add(2,5).subtract(3,2).result()
